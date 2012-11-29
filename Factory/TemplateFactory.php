5ac89056bcd5d46ad17b7e5be6c3eb3cdc1c6374
Jan Hnízdil <jan.hnizdil@dobryweb.cz>
2012-11-29 01:06:42 +0100

    Odstranění presenterové magie z TemplateFactory. Cesta je buď relativní k adresáři se šablonami, nebo absolutní.

--- Factory/TemplateFactory.php
+++ Factory/TemplateFactory.php
@@ -2,65 +2,34 @@
 
 namespace Hnizdil\Factory;
 
-use Nette\Application\IPresenter;
 use Nette\Templating\ITemplate;
 use Hnizdil\Factory\TemplateFactoryException as e;
 
 /**
- * Továrna vytváří šablony ze zadané relativní cesty.
+ * Továrna vytváří šablony. Pokud cesta nezačíná na "/",
+ * vezme se šablona z adresáře se šablonami.
  */
 class TemplateFactory
 {
 
-	private $presenter;
-	private $appDir;
+	private $template;
 	private $templateDir;
 
-	public function __construct(
-		IPresenter $presenter,
-		ITemplate  $template,
-		           $appDir,
-		           $templateDir
-	) {
+	public function __construct(ITemplate $template, $templateDir) {
 
-		$this->presenter   = $presenter;
 		$this->template    = $template;
-		$this->appDir      = $appDir;
 		$this->templateDir = $templateDir;
 
 	}
 
-	public function create($relativePath) {
+	public function create($path) {
 
 		$template = clone $this->template;
 
-		if ($relativePath) {
+		if ($path) {
 
-			// partial
-			if (substr($relativePath, 0, 1) == '_') {
-				$presenterName = $this->presenter->getName();
-				// no module
-				if (($pos = mb_strrpos($presenterName, ':')) === FALSE) {
-					$path = $this->templateDir
-						. '/' . $presenterName
-						. '/' . $relativePath;
-				}
-				// module
-				else {
-					// insert dir "templates" after last module
-					$subPath = substr_replace(
-						$presenterName, 'templates/', $pos + 1, 0);
-					// replace module separator with "Module/"
-					$subPath = str_replace(':', 'Module/', $subPath);
-					// construct path
-					$path = $this->appDir
-						. '/' . $subPath
-						. '/' . $relativePath;
-				}
-			}
-			// template in templates dir
-			else {
-				$path = $this->templateDir . '/' . $relativePath;
+			if (substr($path, 0, 1) !== '/') {
+				$path = $this->templateDir . '/' . $path;
 			}
 
 			if (is_readable($path)) {
